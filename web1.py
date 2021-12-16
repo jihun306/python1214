fe2ec44a612ad2db58fd2e9945cec7fa25956410
jihoon <jeong.jihoon@siemens-heanthineers.com>
2021-12-16 13:35:12 +0900

    클리앙 중고장터

---
+++ web1.py
@@ -0,0 +1,39 @@
+# web1.py
+#웹 크롤링을 위한 선언
+from bs4 import BeautifulSoup
+from bs4.element import PreformattedString
+
+#연습용 파일을 로딩
+#메서드 체인: 메서드를 연속으로 호출
+page = open("c:\\work\\test01.html","rt",encoding="utf-8").read()
+
+#검색이 용이한 객체(HTML태그에서 검색)
+soup = BeautifulSoup(page, "html.parser")
+
+#전체 내용 보기
+# print(soup.prettify())
+
+#<p> 전부 검색 ==> List에 담아서 리턴
+# print(soup.find_all("p"))
+
+#<p>한 개
+# print(soup.find("p"))
+
+#검색조건: <p class="outer-text">
+#파이썬의 예약어(키워드)는 변수명, 인자이름을 사용하면 안됨
+#수많은 <p>에서 필터링(걸러내기)
+# print(soup.find_all("p", class_="outer-text"))
+
+#<p id="first"> 검색
+# print(soup.find_all("p", id="first"))
+
+#내부에 컨텐츠만 가져오기
+result = soup.find_all("p")
+print("갯수:{0}".format(len(result)))
+
+for item in soup.find_all("p"):
+    #태그 내부에 문자열만 리턴
+    content = item.text.strip()
+    content = content.replace("\n","")
+    print(content)
+
---
+++ web2.py
@@ -0,0 +1,26 @@
+# web2.py
+#웹서버와 통신
+import urllib.request
+#크롤링
+from bs4 import BeautifulSoup
+
+#파일에 저장
+f = open("c:\\work\\webtoon.txt","wt",encoding="utf-8")
+
+#동적으로 페이지 번호 생성
+#수열을 생성하는 함수: range(1,6)
+for i in range(1,6):
+
+    url = "https://comic.naver.com/webtoon/list?titleId=20853&weekday=fri&page=" + str(i)
+    print(url)
+    data = urllib.request.urlopen(url)
+    #검색이 용이한 객체
+    soup = BeautifulSoup(data, "html.parser")    
+    cartoons = soup.find_all("td", class_="title")
+    for item in cartoons:
+        title = item.find("a").text.strip()
+        print(title)
+        f.write(title + "\n")
+
+f.close()
+
