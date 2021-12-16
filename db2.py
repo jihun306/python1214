2ba615a17e11aa70888628d3ad0d133a9835cbad
jihoon <jeong.jihoon@siemens-heanthineers.com>
2021-12-16 10:05:11 +0900

    데이터베이스 커밋 연습

--- db2.py
+++ db2.py
@@ -1,17 +1,14 @@
-# db1.py
+# db2.py
 #로컬 데이터베이스 엔진
 import sqlite3
 
-#연결 객체 생성 : 임시로 메모리 저장(Connection)
-con = sqlite3.connect(":memory:")
-
+#연결 객체 생성 : 영구적인 파일에 데이터를 저장
+con = sqlite3.connect("c:\\work\\sample.db")
 #커서에서 실제 SQL 구문을 실행(Cursor클래스)
 cur = con.cursor()
-
 #테이블 구조(테이블 스키마): SQL은 대소문자 구분 안함
 #ANSI SQL 92, 99 표준안
 cur.execute("create table PhoneBook (Name text, PhoneNum text);")
-
 #1건 입력
 cur.execute("insert into PhoneBook values ('derick','010-222');")
 #입력 파라미터 처리
@@ -24,14 +21,7 @@ cur.executemany("insert into PhoneBook values (?,?);",datalist)
 
 #검색
 cur.execute("select * from PhoneBook;")
-# for row in cur:
-#     # print(row[0] + " , " + row[1])
-#     print(row)
-
-print("---fetchone()---")
-print(cur.fetchone())
-print("---fetchmany()---")
-print(cur.fetchmany(2))
-print("---fetchall()---")
-cur.execute("select * from PhoneBook;")
 print(cur.fetchall())
+
+#작업 정상적으로 완료
+con.commit()
